d3f00f5a404cdbc5d4bf9a5081dd62922e034a11
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-04-22 22:29:29 +0200

    Added initial backend express setup

---
+++ server/index.ts
@@ -0,0 +1,26 @@
+import express, { Express, Request, Response } from 'express'
+import dotenv from 'dotenv'
+import helmet from 'helmet'
+import cors from 'cors'
+
+dotenv.config()
+
+const port = process.env.PORT
+const app: Express = express()
+
+app.use(helmet())
+app.use(express.json())
+
+const environment = process.env.NODE_ENV
+const isDevelopment = environment === 'development'
+if (isDevelopment) {
+	app.use(cors())
+}
+
+app.get('/', (req: Request, res: Response) => {
+	res.send('Hello there. 🌃')
+})
+
+app.listen(port, () => {
+	console.log(`Server up and running on port ${port}`)
+})
