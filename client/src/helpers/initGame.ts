ec839b8f5f441001dcefebc15622bb5458ba3c27
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2022-04-23 08:25:54 +0200

    progress on start point of game

--- client/src/helpers/initGame.ts
+++ client/src/helpers/initGame.ts
@@ -1,3 +1,44 @@
 import Phaser from 'phaser'
+import { Config, Scene } from '../models/GameConfig'
+import logoImg from './assets/logo.png'
 
-const config = {}
+class MyGame extends Phaser.Scene {
+	constructor() {
+		super(config)
+	}
+
+	protected preload() {
+		this.load.image('logo', logoImg)
+	}
+
+	protected create() {
+		this.add.image(400, 300, 'sky')
+
+		const particles = this.add.particles('red')
+		const emitter = particles.createEmitter({
+			speed: 100,
+			scale: {
+				start: 1,
+				end: 0,
+			},
+			blendMode: 'add',
+		})
+		const logo = this.physics.add.image(400, 150, 'logo')
+
+		logo.setVelocity(100, 200)
+		logo.setBounce(1, 1)
+		logo.setCollideWorldBounds(true)
+
+		emitter.startFollow(logo)
+	}
+}
+
+const config: Config = {
+	type: Phaser.AUTO,
+	parent: 'phaser-example',
+	width: 800,
+	height: 600,
+	scene: MyGame,
+}
+
+const game = new Phaser.Game(config)
